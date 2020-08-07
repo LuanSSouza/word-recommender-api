c51f1f753867f0ffd65ad191ea14419ceab68138
LuanSSouza <luan.souza27@hotmail.com>
2020-08-07 02:11:39 -0300

    inserindo itens do perfil

--- src/users.py
+++ src/users.py
@@ -9,3 +9,12 @@ def insert_user(user):
 
 def insert_user_stmt(user):
     return "INSERT INTO USER (age,gender,education,usedRecSys,terms_accept) VALUES({0}, {1}, {2}, {3}, {4})".format(user['age'],user['gender'],user['education'],user['usedRecSys'],user['terms_accept'])
+
+def insert_user(user_id, movie: list):
+    with db_connection.connect() as conn:
+        with conn.begin():
+            for m in movie:
+                conn.execute(insert_user_movie_stmt(user_id, m))
+
+def insert_user_movie_stmt(user_id, movie_id):
+    return "INSERT INTO USERMOVIE (user_id, movie_id) VALUES({0}, {1})".format(user_id, movie_id)
