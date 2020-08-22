b3d3f16f12ae783e601c705aef23ed36fe15bdbb
LuanSSouza <luan.souza27@hotmail.com>
2020-08-22 02:07:32 -0300

    fix insercao de avaliacoes

--- src/justifications.py
+++ src/justifications.py
@@ -1,13 +1,12 @@
 from src.connection import db_connection
 
-def insert_rate(user_id, rates: list):
+def insert_rate(user_id, rates: dict):
     with db_connection.connect() as conn:
         with conn.begin():
-            for rate in rates:
-                conn.execute(insert_rate_stmt(user_id,rate))
+            conn.execute(insert_rate_stmt(user_id,rates))
 
 def insert_rate_stmt(user_id, rate):
-    return "INSERT INTO RECJUST (user_id,movie_id,liked,understood,words,interested,confidence,discover) VALUES({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7})".format(user_id, rate['movie_id'],rate['liked'],rate['understood'],rate['words'],rate['interested'],rate['confidence'],rate['discover'])
+    return "INSERT INTO RECCOMP (user_id, reclist1_id, reclist2_id, quality, diversity, serendipity, reclist1, reclist2) VALUES({0}, {1}, {2}, {3}, {4}, {5}, '{6}', '{7}')".format(user_id, rate['reclist1_id'],rate['reclist2_id'],rate['quality'],rate['diversity'],rate['serendipity'],rate['reclist1'],rate['reclist2'])
 
 def insert_comp(user_id, comps: list):
     with db_connection.connect() as conn:
